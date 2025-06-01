6a7ec021191185a6012ed312ac50b73c2563f4a9
akorede <karenbuild@outlook.com>
2025-06-01 11:44:03 +0100

    bye

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HomeView from './components/pages/home/HomeView'
+import Login from './components/pages/auth/Login'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <HomeView />
+     
+
+      <Login />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/pages/home/HomeView.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const HomeView = () => {
+  return (
+    <div className="container ">
+     <h1>Welcome To Awesome Resturant</h1>
+    </div>
+  )
+}
+
+export default HomeView
